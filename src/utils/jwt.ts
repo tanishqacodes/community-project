f5ce359b9823cf12515da9ec872f29d13a2668ad
tanishqacodes <tanishqasharma797@gmail.com>
2023-10-14 20:13:01 +0530

    jwt

---
+++ src/utils/jwt.ts
@@ -0,0 +1,46 @@
+import { JWT_SECRET } from '../config/secret';
+import User from '../interfaces/User.Interface';
+import jwt from 'jsonwebtoken';
+
+export const generateJWT = (user:User):string =>{
+    return jwt.sign(
+        {
+            id : user.id,
+            name : user.name,
+            email : user.email,
+            created_at : user.created_at,
+        },
+        JWT_SECRET,
+        {
+            expiresIn : '3h',
+        }
+    );
+}
+
+export const verifyJWT = (token : string) : User|null =>{
+    try {
+        const user = jwt.verify(token,JWT_SECRET);
+        return user as User;
+    } catch (error) {
+        return null;
+    }
+}
+
+export const verifyJWTMiddleware = (req:any , res:any,next:any){
+    const token = req.headers.authorization?.split(' ')[1];
+    if(!token){
+        return res.status(401).json({
+            success : false,
+            error : "Unauthorized"
+        });
+    }
+    const user = verifyJWT(token);
+    if(!user){
+        return res.status(401).json({
+            success : false,
+            error : "Unauthorized"
+        });
+    }
+    req.user = user;
+    next();
+}
