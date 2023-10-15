e0b9d090300bd7e4c7e1dca6b888eac485b0d683
tanishqacodes <tanishqasharma797@gmail.com>
2023-10-15 11:26:12 +0530

    utils

--- src/utils/bcrypt.ts
+++ src/utils/bcrypt.ts
@@ -2,5 +2,5 @@ import bcrypt from 'bcrypt';
 
 export const hashedPassword = async(password : String):Promise<string> => {
     const salt = await bcrypt.genSalt(10);
-    return bcrypt.hashSync(password,salt);
+    return bcrypt.hash(password,salt);
 }
