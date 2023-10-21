31a52012951d43b258fef2110d3ce910754bd01c
tanishqacodes <tanishqasharma797@gmail.com>
2023-10-21 17:36:22 +0530

    done

--- build/controllers/Member.Controller.js
+++ build/controllers/Member.Controller.js
@@ -69,6 +69,104 @@ const memberController = {
     },
     deleteMember(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
+            const { userId } = req.params;
+            try {
+                const db = (0, db_1.getDatabase)();
+                if (!db) {
+                    return res.status(500).json({
+                        success: false,
+                        error: "Database connection error"
+                    });
+                }
+                const memberCollection = db.collection('member');
+                const memberDetails = yield memberCollection.findOne({ id: userId });
+                if (!memberDetails) {
+                    return res.status(404).json({
+                        success: false,
+                        error: 'Member not found'
+                    });
+                }
+                const result = yield memberCollection.aggregate([
+                    {
+                        $match: {
+                            community: memberDetails.community
+                        },
+                    },
+                    {
+                        $lookup: {
+                            from: 'communities',
+                            localField: 'community',
+                            foreignField: 'id',
+                            as: 'community'
+                        },
+                    },
+                    {
+                        $lookup: {
+                            from: 'users',
+                            localField: 'user',
+                            foreignField: 'id',
+                            as: 'user',
+                        }
+                    },
+                    {
+                        $lookup: {
+                            from: 'roles',
+                            localField: 'role',
+                            foreignField: 'id',
+                            as: 'role',
+                        }
+                    },
+                    {
+                        $unwind: '$role'
+                    },
+                    {
+                        $unwind: '$community'
+                    },
+                    {
+                        $unwind: '$user'
+                    },
+                    {
+                        $project: {
+                            _id: 0,
+                            communityId: '$community.id',
+                            ownerId: '$community.owner',
+                            userId: 'user.id',
+                            role: 'role.name',
+                        },
+                    },
+                ]).toArray();
+                if (result.length === 0) {
+                    return res.status(404).json({
+                        success: false,
+                        error: 'Member not found'
+                    });
+                }
+                const memberData = result[0];
+                const { ownerId, userId: memberId, role: role } = memberData;
+                const requestorUserId = req.user.id;
+                if (requestorUserId !== ownerId.id) {
+                    return res.status(401).json({
+                        success: false,
+                        error: 'NOT_ALLOWED_ACCESS'
+                    });
+                }
+                const deleteResult = yield memberCollection.deleteOne({ id: userId });
+                if (deleteResult.deletedCount === 0) {
+                    return res.status(404).json({
+                        success: false,
+                        error: 'Member not found'
+                    });
+                }
+                res.status(200).json({
+                    success: true
+                });
+            }
+            catch (_a) {
+                res.status(500).json({
+                    success: false,
+                    error: 'Something went wrong'
+                });
+            }
         });
     }
 };
--- build/routes/Member.Routes.js
+++ build/routes/Member.Routes.js
@@ -8,5 +8,5 @@ const jwt_1 = require("../utils/jwt");
 const Member_Controller_1 = __importDefault(require("../controllers/Member.Controller"));
 const router = express_1.default.Router();
 router.post('/add', jwt_1.verifyJWTMiddleware, Member_Controller_1.default.addMember);
-// router.delete('/:id',verifyJWTMiddleware,memberController.deleteMember);
+router.delete('/:id', jwt_1.verifyJWTMiddleware, Member_Controller_1.default.deleteMember);
 exports.default = router;
